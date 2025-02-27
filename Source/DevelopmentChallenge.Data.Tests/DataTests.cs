3a1e33ff1abef8e4583395c8a71a016db24047a1
GG <gororar@gmail.com>
2025-02-27 17:00:27 -0300

    Adding unit tests for 'italian' and 'trapezoid(s)'

--- Source/DevelopmentChallenge.Data.Tests/DataTests.cs
+++ Source/DevelopmentChallenge.Data.Tests/DataTests.cs
@@ -13,21 +13,21 @@ namespace DevelopmentChallenge.Data.Tests
         public void TestResumenListaVacia()
         {
             ClassicAssert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                ReportBuilder.Imprimir(new List<IFormaGeometrica>(), new Castellano()));
+                ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(new List<IFormaGeometrica>()), new Castellano()));
         }
 
         [TestCase]
         public void TestResumenListaVaciaFormasEnIngles()
         {
             ClassicAssert.AreEqual("<h1>Empty list of shapes!</h1>",
-                ReportBuilder.Imprimir(new List<IFormaGeometrica>(), new Ingles()));
+                ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(new List<IFormaGeometrica>()), new Ingles()));
         }
 
         [TestCase]
         public void TestResumenListaVaciaFormasEnItaliano()
         {
             ClassicAssert.AreEqual("<h1>Lista vuota di forme!</h1>",
-                ReportBuilder.Imprimir(new List<IFormaGeometrica>(), new Italiano()));
+                ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(new List<IFormaGeometrica>()), new Italiano()));
         }
 
         [TestCase]
@@ -35,9 +35,29 @@ namespace DevelopmentChallenge.Data.Tests
         {
             var cuadrados = new List<IFormaGeometrica> { new Cuadrado(5) };
 
-            var resumen = ReportBuilder.Imprimir(cuadrados, new Castellano());
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(cuadrados), new Castellano());
 
             ClassicAssert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Área 25 | Perímetro 20 <br/>TOTAL:<br/>1 formas Perímetro 20 Área 25", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConUnCirculo()
+        {
+            var circulos = new List<IFormaGeometrica> { new Circulo(5) };
+
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(circulos), new Castellano());
+
+            ClassicAssert.AreEqual("<h1>Reporte de Formas</h1>1 Círculo | Área 78,54 | Perímetro 31,42 <br/>TOTAL:<br/>1 formas Perímetro 31,42 Área 78,54", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConUnTrapecio()
+        {
+            var trapecio = new List<IFormaGeometrica> { new Trapecio(4, 2, 3, 3.5m, 3.5m) };
+
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(trapecio), new Castellano());
+
+            ClassicAssert.AreEqual("<h1>Reporte de Formas</h1>1 Trapecio | Área 9 | Perímetro 13 <br/>TOTAL:<br/>1 formas Perímetro 13 Área 9", resumen);
         }
 
         [TestCase]
@@ -50,7 +70,7 @@ namespace DevelopmentChallenge.Data.Tests
                 new Cuadrado(3)
             };
 
-            var resumen = ReportBuilder.Imprimir(cuadrados, new Ingles());
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(cuadrados), new Ingles());
 
             ClassicAssert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
         }
@@ -69,10 +89,10 @@ namespace DevelopmentChallenge.Data.Tests
                 new TrianguloEquilatero(4.2m)
             };
 
-            var resumen = ReportBuilder.Imprimir(formas, new Ingles());
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(formas), new Ingles());
 
             ClassicAssert.AreEqual(
-                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13.01 | Perimeter 18.06 <br/>3 Triangles | Area 49.64 | Perimeter 51.6 <br/>TOTAL:<br/>7 shapes Perimeter 97.66 Area 91.65",
+                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 52.03 | Perimeter 36.13 <br/>3 Triangles | Area 49.64 | Perimeter 51.6 <br/>TOTAL:<br/>7 shapes Perimeter 115.73 Area 130.67",
                 resumen);
         }
 
@@ -90,10 +110,29 @@ namespace DevelopmentChallenge.Data.Tests
                 new TrianguloEquilatero(4.2m)
             };
 
-            var resumen = ReportBuilder.Imprimir(formas, new Castellano());
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(formas), new Castellano());
 
             ClassicAssert.AreEqual(
-                "<h1>Reporte de Formas</h1>2 Cuadrados | Área 29 | Perímetro 28 <br/>2 Círculos | Área 13,01 | Perímetro 18,06 <br/>3 Triángulos | Área 49,64 | Perímetro 51,6 <br/>TOTAL:<br/>7 formas Perímetro 97,66 Área 91,65",
+                "<h1>Reporte de Formas</h1>2 Cuadrados | Área 29 | Perímetro 28 <br/>2 Círculos | Área 52,03 | Perímetro 36,13 <br/>3 Triángulos | Área 49,64 | Perímetro 51,6 <br/>TOTAL:<br/>7 formas Perímetro 115,73 Área 130,67",
+                resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTiposEnItaliano()
+        {
+            var formas = new List<IFormaGeometrica>
+            {
+                new Cuadrado( 5),
+                new Circulo(3),
+                new Circulo(3),
+                new TrianguloEquilatero(4),
+                new Trapecio(4,2,3,3.5m,3.5m)
+            };
+
+            var resumen = ReportBuilder.Imprimir(FormaResumenBuilder.CalcularResumen(formas), new Italiano());
+
+            ClassicAssert.AreEqual(
+                "<h1>Report delle Forme</h1>1 Quadrato | Area 25 | Perimetro 20 <br/>2 Cerchi | Area 56,55 | Perimetro 37,7 <br/>1 Triangolo | Area 6,93 | Perimetro 12 <br/>1 Trapezio | Area 9 | Perimetro 13 <br/>TOTALE:<br/>5 forme Perimetro 82,7 Area 97,48",
                 resumen);
         }
     }
